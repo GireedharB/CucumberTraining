f02983a6fd81eefd96fc6d0cfe300b779393492a
GireedharB <gireedharbirajdar1011@gmail.com>
2025-11-20 20:44:45 +0530

    Latest till 20 Nov 2025

--- src/test/java/testRunner/AutomationExerciseRunner.java
+++ src/test/java/testRunner/AutomationExerciseRunner.java
@@ -9,7 +9,7 @@ import org.junit.runner.RunWith;
         features = {"C:\\Users\\ADMIN\\IdeaProjects\\CucumberBDDFramework\\Features\\AutomationExercise.feature"},
         glue = {"stepDefinition","Hooks"},
         dryRun = false,
-        tags = "@Sc4",
+        tags = "@all",
         monochrome = true,
         plugin = {"pretty",
                 "html:target/cucumber-reports.html",
